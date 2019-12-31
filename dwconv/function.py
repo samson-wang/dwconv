e309fb4ea8f265e9857fd52891b348a3978b275f
samson wang <samson.c.wang@gmail.com>
2019-12-31 19:36:06 +0800

    working version

--- dwconv/function.py
+++ dwconv/function.py
@@ -9,19 +9,20 @@ from torch.nn.modules.utils import _pair
 
 class _DepthWiseConv2d(Function):
     @staticmethod
-    def forward(ctx, input, roi, output_size, spatial_scale):
-        ctx.output_size = _pair(output_size)
-        ctx.spatial_scale = spatial_scale
-        ctx.input_shape = input.size()
-        output, argmax = _C.roi_pool_forward(
-            input, roi, spatial_scale, output_size[0], output_size[1]
+    def forward(ctx, input, weight, bias, stride, padding, dilation, groups):
+        #ctx.output_size = _pair(output_size)
+        #ctx.spatial_scale = spatial_scale
+        #ctx.input_shape = input.size()
+        output = _C.depth_wise_conv2d(
+            input, weight, bias, stride, padding, dilation, groups
         )
-        ctx.save_for_backward(input, roi, argmax)
+        #ctx.save_for_backward(input, roi, argmax)
         return output
 
     @staticmethod
     @once_differentiable
     def backward(ctx, grad_output):
+        raise
         input, rois, argmax = ctx.saved_tensors
         output_size = ctx.output_size
         spatial_scale = ctx.spatial_scale
@@ -42,21 +43,21 @@ class _DepthWiseConv2d(Function):
         return grad_input, None, None, None
 
 
-roi_pool = _DepthWiseConv2d.apply
+depth_wise_conv2d = _DepthWiseConv2d.apply
 
 
-class DepthWiseConv2d(nn.Module):
-    def __init__(self, output_size, spatial_scale):
-        super(DepthWiseConv2d, self).__init__()
-        self.output_size = output_size
-        self.spatial_scale = spatial_scale
-
-    def forward(self, input, rois):
-        return roi_pool(input, rois, self.output_size, self.spatial_scale)
-
-    def __repr__(self):
-        tmpstr = self.__class__.__name__ + "("
-        tmpstr += "output_size=" + str(self.output_size)
-        tmpstr += ", spatial_scale=" + str(self.spatial_scale)
-        tmpstr += ")"
-        return tmpstr
+#class DepthWiseConv2d(nn.Module):
+#    def __init__(self, output_size, spatial_scale):
+#        super(DepthWiseConv2d, self).__init__()
+#        self.output_size = output_size
+#        self.spatial_scale = spatial_scale
+#
+#    def forward(self, input, rois):
+#        return roi_pool(input, rois, self.output_size, self.spatial_scale)
+#
+#    def __repr__(self):
+#        tmpstr = self.__class__.__name__ + "("
+#        tmpstr += "output_size=" + str(self.output_size)
+#        tmpstr += ", spatial_scale=" + str(self.spatial_scale)
+#        tmpstr += ")"
+#        return tmpstr
